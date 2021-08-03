de587608a2b07c2280a703521f51f6f7db27cbde
LFelixF <luckasfelix@hotmail.com>
2021-08-03 18:31:58 -0300

    desafio 3-2

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id: "mapas-com-react-usando-leaflet",
+        image_url: "https://blog.rocketseat.com.br/content/images/2020/11/blog-thumb-utilizando-mapas-no-react-com-leaflet-1.jpg",
+        title: "Mapas com React usando Leaflet",
+        author: "by: Thiago Marinho",
+        duration: "11 MIN DE LEITURA"
+    },
+    {
+        id: "como-renomear-varios-arquivos-de-uma-vez-usando-o-terminal",
+        image_url: "https://blog.rocketseat.com.br/content/images/2020/11/thumb-como-renomear-varios-arquivos-de-uma-vez-usando-o-terminal.jpg",
+        title: "Como renomear vários arquivos de uma vez usando o terminal",
+        author: "by: Thiago Marinho",
+        duration: "2 MIN DE LEITURA"
+    },
+    {
+        id: "construindo-um-app-com-mapas-usando-react-native-maps-e-mapbox",
+        image_url: "https://blog.rocketseat.com.br/content/images/2020/10/teste.jpg",
+        title: "Construindo App com Mapa usando React Native Maps e MapBox",
+        author: "by: Thiago Marinho",
+        duration: "8 MIN DE LEITURA"
+    }
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const nunjucks = require("nunjucks")
+const articles = require("./data")
 
 const server = express()
 
@@ -16,11 +17,31 @@ nunjucks.configure("views", {
 server.use(express.static("public"))
 
 server.get("/", (req, res) => {
-    return res.render("about")
+    const data = {
+        avatar_url: "https://cdn-images-1.medium.com/max/1200/1*TkXVfLTwsHdwpUEjGzdi9w.jpeg",
+        name: "Rocketseat",
+        discription: "Nossa missão é capacitar pessoas que buscam se profissionalizar na programação independente do seu momento ou nível de conhecimento.",
+        list: {
+            name: "Principais Tecnologias",
+            technologies: [
+                {name: "HMTL"},
+                {name: "CSS"},
+                {name: "Javascript"},
+                {name: "Node.js"}
+            ]
+        },
+        links: [
+            {name: "Facebook", url: ""},
+            {name: "Instagram", url: ""},
+            {name: "Github", url: ""}
+        ]
+    }
+
+    return res.render("about", {about: data})
 })
 
 server.get("/content", (req, res) => {
-    return res.render("content")
+    return res.render("content", {items: articles})
 })
 
 server.use(function(req, res) {
